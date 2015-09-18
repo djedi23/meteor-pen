4238888f25e3c1cb2625526a08320ccb9b6f8314
Moïse Valvassori <moise.valvassori@gmail.com>
2015-09-18 06:02:24 +0200

    upgrade to pen:0.2.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'djedi:pen',
-  version: '0.1.0',
+  version: '0.2.2',
   // Brief, one-line summary of the package.
   summary: 'Enjoy live editing with Meteor',
   // URL to the Git repository containing the source code for this package.
